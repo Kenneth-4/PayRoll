db9b42c1a201cfdbdf2a529a5ae5dd1e24fb68d4
visxge <cubian.kenneth.4@gmail.com>
2024-08-06 11:36:46 +0800

    Kenneth

--- PayRoll.py
+++ PayRoll.py
@@ -1,8 +1,12 @@
 import tkinter as tk
+from tkinter import Label, font
 
 main = tk.Tk()
 
-main.geometry("1200x600")
+main.geometry("900x250")
 main.title("PayRoll")
+
+welcome = tk.Label(main, text='WELCOME')
+welcome.pack(pady=20)
 
 main.mainloop()
